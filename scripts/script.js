f859a7ebf95c4891936c546ead1d66c7c465b681
isaacrez <isaacrez20@gmail.com>
2020-09-04 14:30:54 -0400

    Refactor operation

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+function operate(type, a, b) {
+    switch(type) {
+        case 'add':
+            return add(a, b);
+        case 'subtract':
+            return subtract(a, b);
+        case 'multiply':
+            return multiply(a, b);
+        case 'divide':
+            return divide(a, b);
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}    
+
+module.exports = operate;
--- scripts/spec/scriptSpec.js
+++ scripts/spec/scriptSpec.js
@@ -1,30 +1,26 @@
 
-const operation = require('../script.js');
+const operate = require('../script.js');
 
 describe('add', () => {
     it('adds two numbers', () => {
-        let operator = new operation();
-        expect(operator.add(3, 5)).toBe(8);
+        expect(operate('add', 3, 5)).toBe(8);
     })
 })
 
 describe('subtract', () => {
     it('subtracts two numbers', () => {
-        let operator = new operation();
-        expect(operator.subtract(3, 5)).toBe(-2);
+        expect(operate('subtract', 3, 5)).toBe(-2);
     })
 })
 
 describe('division', () => {
     it('divides two numbers', () => {
-        let operator = new operation();
-        expect(operator.divide(3, 5)).toBe(3/5);
+        expect(operate('divide', 3, 5)).toBe(3/5);
     })
 })
 
 describe('multiplication', () => {
     it('multiplies two numbers', () => {
-        let operator = new operation();
-        expect(operator.multiply(3, 5)).toBe(15);
+        expect(operate('multiply', 3, 5)).toBe(15);
     })
 })
