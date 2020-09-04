2962a3a8de643c30b07436616e448e842b612460
isaacrez <isaacrez20@gmail.com>
2020-09-04 16:00:32 -0400

    Implement numeric button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -25,6 +25,32 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
-}    
+}
+
+let storedValue = 0;
+let displayValue = '';
+
+let display = document.querySelector('#display p');
+display.textContent = displayValue;
+
+let numberBin = document.querySelector('#numbers');
+let numbers = [...numberBin.children];
+
+function updateDisplay(e) {
+    let number = this.textContent;
+    if (invalidNumericInput(number)) {
+        displayValue === '0' ? displayValue = number : displayValue += number;
+        display.textContent = displayValue;    
+    }
+}
+
+function invalidNumericInput(number) {
+    return (number === '0' && displayValue === '0') ? false : true;
+}
+
+numbers.forEach(number => {
+    number.addEventListener('click', updateDisplay)
+})
+
 
 module.exports = operate;
