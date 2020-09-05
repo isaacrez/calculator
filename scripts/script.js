6f5466fbd1bb1f6d84269d1a3706300f97787812
isaacrez <isaacrez20@gmail.com>
2020-09-05 14:59:28 -0400

    Complete calculator functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 function operate(type, a, b) {
+    a = Number(a);
+    b = Number(b);
     switch(type) {
         case 'add':
             return add(a, b);
@@ -27,24 +29,122 @@ function divide(a, b) {
     return a / b;
 }
 
-let storedValue = 0;
-let displayValue = '0';
+let storedValue;
+let displayValue = ' ';
+let state = 'initial';
+let operator;
 
 let display = document.querySelector('#display p');
 display.textContent = displayValue;
 
-let numberBin = document.querySelector('#numbers');
-let numbers = [...numberBin.children];
+function extendDisplayValue(e) {
+    state === 'initial' ? state = 'initial' : state = 'numeric';
 
-function updateDisplay(e) {
-    let number = this.textContent;
-    displayValue === '0' ? displayValue = number : displayValue += number;
-    display.textContent = displayValue;    
+    let input = this.textContent;
+    if (notOverflowing()) {
+        updateDisplayValue(input);
+    }
+    updateDisplay();
 }
 
-numbers.forEach(number => {
-    number.addEventListener('click', updateDisplay)
-})
+function notOverflowing() {
+    return displayValue.length < 22;
+}
 
+function updateDisplayValue(input) {
+    if (displayValue.includes('.') && input === '.') {
+        return;
+    }
+    displayValue === '0' ? displayValue = input : displayValue += input;
+}
+
+function undoDisplayValue() {
+    let n = displayValue.length - 1;
+    if (0 < n) {
+        displayValue = displayValue.substr(0, n);
+    } else {
+        displayValue = '0';
+    }
+    updateDisplay();
+}
+
+function updateDisplay(value=displayValue) {
+    display.textContent = value;
+}
+
+let numbers = [...document.querySelector('#numbers').children];
+numbers.forEach(number => number.addEventListener('click', extendDisplayValue));
+
+function processOperatorPress(e) {
+
+    console.log(state);
+    let newOperator = this.value;
+    if (newOperator === 'equals') {
+        processEquals();
+        return;
+    }
+
+    switch (state) {
+        case 'initial':
+            storedValue = displayValue;
+            partialReset(newOperator);
+
+            state = 'operator';
+            break;
+            
+        case 'operator':
+            operator = newOperator;
+            break;
+
+        case 'numeric':
+            storedValue = operate(operator, storedValue, displayValue);
+            partialReset(newOperator);
+            updateDisplay(storedValue);
+            
+            state = 'operator';
+            break;
+    }
+
+    console.log(`${state}: ${storedValue} ${operator} ${displayValue}`);
+}
+
+function processEquals() {
+    storedValue = operate(operator, storedValue, displayValue);
+    displayValue = ' ';
+    state = 'operator';
+    operator = 'none';
+    updateDisplay(storedValue);
+}
+
+function partialReset(newOperator) {
+    updateDisplay();
+    displayValue = ' ';
+    operator = newOperator;
+}
+
+let operatorButtons = [...document.querySelector('#operators').children];
+operatorButtons.forEach(button => button.addEventListener('click', processOperatorPress));
+
+function auxillaryPress(e) {
+    let auxCall = this.value;
+    switch (auxCall) {
+        case 'clear':
+            fullClear();
+            break;
+        case 'delete':
+            undoDisplayValue();
+            break;
+    }
+}
+
+function fullClear() {
+    storedValue = null;
+    state = 'initial';
+    partialReset('none');
+    updateDisplay();
+}
+
+let auxButtons = [...document.querySelector('#auxillary').children];
+auxButtons.forEach(button => button.addEventListener('click', auxillaryPress));
 
 module.exports = operate;
